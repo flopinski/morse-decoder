dcd21f6d3401da0522c84383413199831fcde3b7
flopinski <103384762+flopinski@users.noreply.github.com>
2022-08-02 18:36:16 +0300

    feat: done with this

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = expr.match(/.{1,10}/g);
+
+    for (let i = 0; i < result.length; i++) {
+      if (result[i].includes("*")) continue;
+      else result[i] = parseInt(result[i], 10);
+    }
+    //  let divided = (result[0]).toString().match(/.{1,2}/g)
+
+    const sweeterArray = result.map((element) => {
+      return element.toString().match(/.{1,2}/g);
+    });
+
+    let res = [];
+
+    for (let k = 0; k < sweeterArray.length; k++) {
+      for (let j = 0; j < sweeterArray[k].length; j++) {
+        if (sweeterArray[k][j] === "10") sweeterArray[k][j] = ".";
+        if (sweeterArray[k][j] === "11") sweeterArray[k][j] = "-";
+        if (sweeterArray[k][j] === "**") sweeterArray[k][j] = " ";
+      }
+    }
+
+    let arr = sweeterArray.map((element) => {
+      return element.join("");
+    });
+
+    let morse = arr.map((el) => MORSE_TABLE[el]);
+
+    morse.forEach((item, index) => {
+      if (item === undefined) morse[index] = " ";
+    });
+
+    morse = morse.join("");
+    return morse
 }
 
 module.exports = {
